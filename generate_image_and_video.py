aaf87bb9816e1683da6d83f2cf5518ca36a7e374
Chris Portka <chrisportka@gmail.com>
2023-04-21 00:06:31 -0700

    Bug fix, optional GPU support
    - if conda is installed, the script will attempt to use it for gpu
      otherwise it will use your cpu
    - fixed a bug with un-sanitized characters in the prompt

--- generate_image_and_video.py
+++ generate_image_and_video.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import subprocess
 import torch
+import re
 from diffusers import StableDiffusionPipeline
 from transformers import CLIPImageProcessor
 
@@ -17,19 +18,21 @@ def generate_image(prompt, device, iteration, seed=None):
 
     image = pipe(prompt).images[0]
 
-    filename = f"{prompt.replace(' ', '_')}_generated_image_{iteration}.png"
+    sanitized_prompt = re.sub(r'\W+', '_', prompt)
+    filename = f"{sanitized_prompt}_generated_image_{iteration}.png"
     image.save(filename)
     return filename
 
-def create_video(filenames, prompt, fps=24):
-    output_name = f"{prompt.replace(' ', '_')}_generated_video.mp4"
-    duration = 1 / fps
-    ffmpeg_cmd = ["ffmpeg", "-y", "-f", "concat", "-i", "file_list.txt", "-c:v", "libx264", "-profile:v", "high", "-crf", "20", "-pix_fmt", "yuv420p", output_name]
+def create_video(filenames, prompt, fps=1):
+    sanitized_prompt = re.sub(r'\W+', '_', prompt)
+    output_name = f"{sanitized_prompt}_generated_video.mp4"
 
     with open("file_list.txt", "w") as f:
         for filename in filenames:
-            f.write(f"file '{filename}'\n")
-            f.write(f"duration {duration}\n")
+            f.write(f"file '{filename}'\nduration {1/fps}\n")
+        f.write(f"file '{filenames[-1]}'\n")  # Add the last image without duration to fix end time
+
+    ffmpeg_cmd = ["ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", "file_list.txt", "-c:v", "libx264", "-profile:v", "high", "-crf", "20", "-pix_fmt", "yuv420p", "-r", str(fps), output_name]
 
     subprocess.run(ffmpeg_cmd)
 
@@ -37,19 +40,14 @@ def create_video(filenames, prompt, fps=24):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate images from text prompt.")
-    parser.add_argument("prompt", type=str, nargs='?', default=None, help="Text prompt for image generation.")
+    parser.add_argument("prompt", type=str, help="Text prompt for image generation.")
     parser.add_argument("--iterations", type=int, default=1, help="Number of images to generate.")
     parser.add_argument("--seed", type=int, default=None, help="Random seed for image generation.")
     parser.add_argument("--fps", type=int, default=24, help="Frames per second for generated video.")
 
     args = parser.parse_args()
 
-    if args.prompt is None:
-        print("Error: No prompt provided.")
-        parser.print_help()
-        exit(1)
-
-    device = "cpu"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     prompt = args.prompt
     iterations = args.iterations
     seed = args.seed
